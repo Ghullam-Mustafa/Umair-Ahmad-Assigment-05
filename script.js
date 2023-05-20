af1cbc4661280657a4eb72a44dfe705a6f24dfba
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-05-20 23:45:59 +0500

    ceilANumber

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 // --------------------------Round A Number---------------------------------
 document.getElementById('roundANumber').onclick = function () {
-    
-    var num1 = 10.03;
-    var num2 = 54.18;
-    var num3 = 354.19;
+    alert("Value gives with points")
+    var num1 = +prompt("enter a first value");
+    var num2 = +prompt("enter a second value");
+    var num3 = +prompt("enter a third value");
 
     alert( `first value ${num1} , second value ${num2} , third ${num3} this all value is added`);
 
     var totalPrice = num1 + num2 + num3 ;
     totalPrice = Math.round(totalPrice)
+
+    document.getElementById('output').innerHTML = totalPrice;
+}
+
+// ------------------------------------ceilANumber================================
+document.getElementById('ceilANumber').onclick = function () {
+    alert("Value gives with points")
+    var num1 = +prompt("enter a first value");
+    var num2 = +prompt("enter a second value");
+    var num3 = +prompt("enter a third value");
+
+    alert( `first value ${num1} , second value ${num2} , third ${num3} this all value is added`);
+
+    var totalPrice = num1 + num2 + num3 ;
+    totalPrice = Math.ceil(totalPrice)
 
     document.getElementById('output').innerHTML = totalPrice;
 }
